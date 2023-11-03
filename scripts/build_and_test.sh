23246e0402a08941d722243867d127b4f4469e73
Marcio Carvalho <camarcio@amazon.com>
2023-11-03 14:52:09 -0700

    Fix cargo deny issues and enables cargo deny on ci/cd

---
+++ scripts/build_and_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rustup update stable && rustup default stable && \
+cargo fmt --all --check && \
+RUSTFLAGS="-D warnings -F unsafe-code" cargo build --verbose && \
+cargo doc --all-features --no-deps && \
+cargo clippy --all-features && \
+cargo test --verbose && \
+# cargo test --verbose --features integration-tests && \
+cargo test --verbose --no-default-features && \
+cargo test --verbose -- --ignored && \
+cargo bench --no-run && \
+cargo audit --deny warnings && \
+cargo deny check
